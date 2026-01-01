110a06ae676d87845f184178fbdfcb3d494f2903
Apishrana <apishrana682@gmail.com>
2026-01-01 09:13:13 +0530

    Card css hotfix

---
+++ Src/Loadcards.js
@@ -0,0 +1,140 @@
+const houseList = [
+    {
+        id: 1,
+        name: 'Temp',
+        location: 'Blr, India',
+        price: '28,000',
+        specifications: '2 BHK',
+        healthscore: 82,
+        image: '../assets/houses/house1.png',
+    },
+    {
+        id: 2,
+        name: 'Temp',
+        location: 'Blr, India',
+        price: '40,000',
+        specifications: '3 BHK',
+        healthscore: 90,
+        image: '../assets/houses/house2.png',
+    },
+    {
+        id: 3,
+        name: 'Temp',
+        location: 'Blr, India',
+        price: '18,000',
+        specifications: '1 BHK',
+        healthscore: 50,
+        image: '../assets/houses/house3.png',
+    },
+    {
+        id: 4,
+        name: 'Temp',
+        location: 'Blr, India',
+        price: '30,000',
+        specifications: '3 BHK',
+        healthscore: 82,
+        image: '../assets/houses/house4.png',
+    },
+    {
+        id: 5,
+        name: 'Temp',
+        location: 'Blr, India',
+        price: '22,000',
+        specifications: '1 BHK',
+        healthscore: 65,
+        image: '../assets/houses/house5.png',
+    },
+    {
+        id: 6,
+        name: 'Temp',
+        location: 'Blr, India',
+        price: '35,000',
+        specifications: '2 BHK',
+        healthscore: 88,
+        image: '../assets/houses/house6.png',
+    },
+    {
+        id: 7,
+        name: 'Temp',
+        location: 'Blr, India',
+        price: '45,000',
+        specifications: '3 BHK',
+        healthscore: 92,
+        image: '../assets/houses/house7.png',
+    },
+    {
+        id: 8,
+        name: 'Temp',
+        location: 'Blr, India',
+        price: '16,000',
+        specifications: 'Studio',
+        healthscore: 48,
+        image: '../assets/houses/house8.png',
+    },
+    {
+        id: 9,
+        name: 'Temp',
+        location: 'Blr, India',
+        price: '26,000',
+        specifications: '2 BHK',
+        healthscore: 72,
+        image: '../assets/houses/house9.png',
+    },
+    {
+        id: 10,
+        name: 'Temp',
+        location: 'Blr, India',
+        price: '55,000',
+        specifications: '4 BHK',
+        healthscore: 95,
+        image: '../assets/houses/house10.png',
+    },
+    {
+        id: 11,
+        name: 'Temp',
+        location: 'Blr, India',
+        price: '14,000',
+        specifications: 'Studio',
+        healthscore: 42,
+        image: '../assets/houses/house11.png',
+    },
+    {
+        id: 12,
+        name: 'Temp',
+        location: 'Blr, India',
+        price: '32,000',
+        specifications: '2 BHK',
+        healthscore: 80,
+        image: '../assets/houses/house12.png',
+    },
+];
+
+const usedCardId = [];
+
+const cardElements = document.getElementsByClassName('card');
+
+for (let i = 0; i < cardElements.length; i++) {
+    let randomHouse = getRandomHouse();
+    let currentCard = cardElements[i];
+    currentCard.getElementsByClassName('card-img')[0].src = randomHouse.image;
+    currentCard.getElementsByClassName('card-img')[0].alt = randomHouse.image;
+    currentCard
+        .getElementsByClassName('card-disc')[0]
+        .getElementsByClassName('card-info')[0].innerText =
+        '₹ ' + randomHouse.price + ' · ' + randomHouse.specifications;
+    currentCard
+        .getElementsByClassName('card-disc')[0]
+        .getElementsByClassName('card-health')[0].innerText =
+        'Health Score: ' + randomHouse.healthscore;
+}
+
+function getRandomHouse() {
+    let randomIndex = Math.floor(Math.random() * houseList.length);
+    let selectedHouse = houseList[randomIndex];
+
+    if (usedCardId.includes(selectedHouse.id)) {
+        return getRandomHouse();
+    }
+    usedCardId.push(selectedHouse.id);
+    return selectedHouse;
+}
